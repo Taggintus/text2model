83cd2200718ad9135415b4abb50c08e709f2bdd5
Stefan <psychoazubi@googlemail.com>
2014-12-08 22:39:14 +0100

    Erste Tests mit WordNet

--- src/Wordnet/Wordnet.java
+++ src/Wordnet/Wordnet.java
@@ -1,5 +1,26 @@
 package Wordnet;
 
+
+import edu.smu.tspell.wordnet.*;
+import edu.smu.tspell.wordnet.impl.*;
+import edu.smu.tspell.wordnet.impl.file.*;
+import edu.smu.tspell.wordnet.impl.file.synset.*;
+
+
 public class Wordnet {
 
+	public static void main(String[] args)
+	{
+		//Datenbank erstellen
+		WordNetDatabase database = WordNetDatabase.getFileInstance();
+		
+		Synset[] nounSynset;		//Synonyme sammlen
+		NounSynset[] hyponyms;		//Ober/Unterbegriffe sammeln
+		int i;
+		//Synonyme erhalten
+		for (i=0; i < args.length; i++) {
+		
+			nounSynset =  database.getSynsets(args[i]);
+		}
+	}
 }

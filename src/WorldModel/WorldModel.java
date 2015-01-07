9d14472be984c537c452e6a3cf204e9f3d2329e6
Stefan <psychoazubi@googlemail.com>
2015-01-07 22:55:11 +0100

    WorldModel added

---
+++ src/WorldModel/WorldModel.java
@@ -0,0 +1,21 @@
+package WorldModel;
+
+import java.util.ArrayList;
+
+
+/** Eintritt in World Model; Über das World Model werden die Sätze zerlegt.
+ * Im World Model gibt es 4 Hauptelemente: Actor, Resource, Action und Flow (s. fig. 10 in Friedrich Master Thesis)
+ */
+
+public class WorldModel {
+	
+	private ArrayList<Actor> actors = new ArrayList<Actor>();
+	
+	
+	//Erstellt eine Liste mit allen Actors
+	public void addActor ( ) {
+		
+	}
+}
+
+

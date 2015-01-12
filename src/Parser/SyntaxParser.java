61a467227bca3044eaf54884ba6a4ff57bef176e
Andreas Maring <s9anmari@stud.uni-saarland.de>
2015-01-12 15:39:43 +0100

    trying to import the world model

--- src/Parser/SyntaxParser.java
+++ src/Parser/SyntaxParser.java
@@ -1,14 +1,5 @@
 package Parser;
 
-import java.io.StringReader;
-import java.util.List;
-
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.process.TokenizerFactory;
-import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.process.Tokenizer;
 import edu.stanford.nlp.trees.Tree;
 
 /*

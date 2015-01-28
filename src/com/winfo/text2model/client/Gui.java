7f9e2ce910f88524da454dc03a77005e8442255d
Aggintus <Aggintus@gmail.com>
2015-01-28 23:39:21 +0100

    Gui abgekapselt, Input- und Outputfeld, Eingabe wird testweise reversiert und mit entsprechenden Attributen ausgegeben.

---
+++ src/com/winfo/text2model/client/Gui.java
@@ -0,0 +1,121 @@
+package com.winfo.text2model.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Gui extends Composite{
+	
+	private HorizontalPanel mainPanel = new HorizontalPanel();
+	private HorizontalPanel outputPanel = new HorizontalPanel();
+	private VerticalPanel buttonPanel = new VerticalPanel();
+	private TextArea textInput = new TextArea();
+	private TextArea textOutput = new TextArea();
+	
+	private Button sendButton = new Button("Send");
+	private RadioButton bpmnb = new RadioButton("modeltype", "BPMN");
+    private RadioButton epcb = new RadioButton("modeltype", "EPC");
+	private RadioButton engb = new RadioButton("language", "English");
+    private RadioButton gerb = new RadioButton("language", "Deutsch");
+    
+    int t = 1;
+    int l = 1;
+    
+	public Gui() {
+		
+		initWidget(this.mainPanel);
+
+	    // Assemble send panel.
+	    mainPanel.add(textInput);
+	    textInput.setCharacterWidth(100);
+	    textInput.setVisibleLines(25);
+	    
+	    mainPanel.add(buttonPanel);
+	    
+	    // Assemble output panel.
+	    mainPanel.add(outputPanel);
+	    textOutput.setCharacterWidth(100);
+	    textOutput.setVisibleLines(25);
+	    
+	    
+	    outputPanel.add(textOutput);
+	    
+	    // Assemble Buttons
+	    buttonPanel.add(bpmnb);
+	    buttonPanel.add(epcb);
+	    buttonPanel.add(engb);
+	    buttonPanel.add(gerb);
+	    buttonPanel.add(sendButton);
+	    
+	    // Set standard button
+	    bpmnb.setChecked(true);
+	    engb.setChecked(true);
+	    
+	    // Associate the Main panel with the HTML host page.
+	   // RootPanel.get("text2model").add(mainPanel);
+	    
+	    // Move cursor focus to the input box.
+	    textInput.setFocus(true);
+
+	    
+	    // Listen for mouse events on the Add button.
+	    sendButton.addClickHandler(new ClickHandler() {
+	      public void onClick(ClickEvent event) {
+	        sendtext();
+	      }
+	    });
+	    
+	    // Listen for keyboard events in the input box.
+	    textInput.addKeyDownHandler(new KeyDownHandler() {
+	      public void onKeyDown(KeyDownEvent event) {
+	        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+	          sendtext();
+	        }
+	      }
+	    });
+	
+	}
+	
+	/**  * Save input text as string */ 
+	private void sendtext() {
+	    String text = textInput.getText();
+	    textInput.setFocus(true);
+	    
+	    // Save the modeltype in t // 1 = BPMN; 0 = EPK
+	    if (bpmnb.isChecked()) {
+	    	t = 1;
+	    } else {
+	    	t = 0 ;
+	    }
+	    
+	    // Save the language in l // 1 = english; 0 = german
+	    if (engb.isChecked()) {
+	    	l = 1;
+	    } else {
+	    	l = 0 ;
+	    }
+	    
+	    String ss;
+	    String sl;
+	    
+	    if (l==1) {ss="english";} else {ss="Deutsch";}
+	    if (t==1) {sl="BPMN";} else {sl="EPK";}
+	    
+	    text = new StringBuffer(text).reverse().toString();
+	    
+	    String s = "Ihre reversierte Eingabe: "+text +"\n"+"Gewaehlte Sprache: "+ss+"\n"+"Gewaehltes Model: "+sl;
+	    
+	    textOutput.setText(s);
+	    
+	 }
+	
+}
--- src/com/winfo/text2model/client/Text2Model.java
+++ src/com/winfo/text2model/client/Text2Model.java
@@ -15,86 +15,14 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 /** * Entry point classes define <code>onModuleLoad()</code>. */
 public class Text2Model implements EntryPoint {
-	
-	private VerticalPanel mainPanel = new VerticalPanel();
-	private HorizontalPanel addPanel = new HorizontalPanel();
-	private TextArea newTextArea = new TextArea();
-	private Button sendButton = new Button("Send");
-	private RadioButton bpmnb = new RadioButton("modeltype", "BPMN");
-    private RadioButton epcb = new RadioButton("modeltype", "EPC");
-	private RadioButton engb = new RadioButton("language", "English");
-    private RadioButton gerb = new RadioButton("language", "Deutsch");
-    int t = 1;
-    int l = 1;
-
 
 /**  * Entry point method.  */  
 public void onModuleLoad() {
-	
-	
-    // Assemble send panel.
-    addPanel.add(newTextArea);
-    newTextArea.setCharacterWidth(100);
-    newTextArea.setVisibleLines(25);
-    
-    addPanel.add(sendButton);
-	
-    // Assemble Main panel.
-    mainPanel.add(addPanel);
-    
-    // Assemble Radio Buttons
-    mainPanel.add(bpmnb);
-    mainPanel.add(epcb);
-    mainPanel.add(engb);
-    mainPanel.add(gerb);
-    
-    // Set standard button
-    bpmnb.setChecked(true);
-    engb.setChecked(true);
-    
-    // Associate the Main panel with the HTML host page.
-    RootPanel.get("text2model").add(mainPanel);
-    
-    // Move cursor focus to the input box.
-    newTextArea.setFocus(true);
 
-    
-    // Listen for mouse events on the Add button.
-    sendButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        sendtext();
-      }
-    });
-    
-    // Listen for keyboard events in the input box.
-    newTextArea.addKeyDownHandler(new KeyDownHandler() {
-      public void onKeyDown(KeyDownEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendtext();
-        }
-      }
-    });
-
+	Gui gui = new Gui();
+	
+	RootPanel.get().add(gui);
 
 }
 
-/**  * Save input text as string */ 
-private void sendtext() {
-    final String text = newTextArea.getText();
-    newTextArea.setFocus(true);
-    
-    // Save the modeltype in t // 1 = BPMN; 0 = EPK
-    if (bpmnb.isChecked()) {
-    	t = 1;
-    } else {
-    	t = 0 ;
-    }
-    
-    // Save the language in l // 1 = english; 0 = german
-    if (engb.isChecked()) {
-    	l = 1;
-    } else {
-    	l = 0 ;
-    }
- }
 }

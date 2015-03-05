7da7410531825e39ade490fa6953c2679af3be12
Stefan <psychoazubi@googlemail.com>
2015-03-05 13:21:56 +0100

    neue Builderklassen

---
+++ src/transform/EPCModelBuilder.java
@@ -0,0 +1,36 @@
+package transform;
+
+import worldModel.Action;
+import worldModel.WorldModel;
+import Models.ProcessModel;
+import etc.TextToProcess;
+
+public class EPCModelBuilder extends ProcessModelBuilder {
+
+	public EPCModelBuilder(TextToProcess parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ProcessModel createProcessModel(WorldModel world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void buildDataObjects(WorldModel world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DataObject createDataObject(Action targetAc, String doName,
+			boolean arriving) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}

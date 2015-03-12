455b1afa74bd8dac2b3d762521ef2b70e42c8add
Tobias Aggintus <T@10.9.34.170>
2015-03-12 12:00:22 +0100

    fixed warnings // added comments

--- src/com/winfo/text2model/client/Gui.java
+++ src/com/winfo/text2model/client/Gui.java
@@ -12,126 +12,127 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RadioButton;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class Gui extends Composite{
-	
+public class Gui extends Composite {
+
+	// Initiate Panels
 	private HorizontalPanel mainPanel = new HorizontalPanel();
 	private HorizontalPanel outputPanel = new HorizontalPanel();
 	private VerticalPanel buttonPanel = new VerticalPanel();
 	private TextArea textInput = new TextArea();
 	private TextArea textOutput = new TextArea();
-	
+
+	// Initiate Buttons
 	private Button sendButton = new Button("Send");
 	private RadioButton bpmnb = new RadioButton("modeltype", "BPMN");
-    private RadioButton epcb = new RadioButton("modeltype", "EPC");
+	private RadioButton epcb = new RadioButton("modeltype", "EPC");
 	private RadioButton engb = new RadioButton("language", "English");
-    private RadioButton gerb = new RadioButton("language", "Deutsch");
-    
-    int t = 1;
-    int l = 1;
-    
-    private MessageServiceAsync messageService = 
-    		   GWT.create(MessageService.class);
+	private RadioButton gerb = new RadioButton("language", "Deutsch");
 
-    		   private class MessageCallBack implements AsyncCallback<Message> {
-    		      @Override
-    		      public void onFailure(Throwable caught) {
-    		         /* server side error occured */
-    		         Window.alert("Unable to obtain server response: " 
-    		         + caught.getMessage());	
-    		      }
-    		      @Override
-    		      public void onSuccess(Message result) {
-    		          /* server returned result, show user the message */
-    		    	    
-    		  	    textOutput.setText(result.getMessage());
-    		         Window.alert(result.getMessage());
-    		      }	   
-    		   }
-    
+	// Initiate variables for type and language
+	int t = 1;
+	int l = 1;
+
+	// Async part of the rpc
+	private MessageServiceAsync messageService = GWT
+			.create(MessageService.class);
+
+	private class MessageCallBack implements AsyncCallback<Message> {
+		@Override
+		public void onFailure(Throwable caught) {
+			/* server side error occured */
+			Window.alert("Unable to obtain server response: "
+					+ caught.getMessage());
+		}
+
+		@Override
+		public void onSuccess(Message result) {
+			/* server returned result, show user the message */
+
+			textOutput.setText(result.getMessage());
+			Window.alert(result.getMessage());
+		}
+	}
+
+	// Initiate Gui
+	@SuppressWarnings("deprecation")
 	public Gui() {
-		
+
 		initWidget(this.mainPanel);
 
-	    // Assemble send panel.
-	    mainPanel.add(textInput);
-	    textInput.setCharacterWidth(100);
-	    textInput.setVisibleLines(25);
-	    
-	    mainPanel.add(buttonPanel);
-	    
-	    // Assemble output panel.
-	    mainPanel.add(outputPanel);
-	    textOutput.setCharacterWidth(100);
-	    textOutput.setVisibleLines(25);
-	    
-	    
-	    outputPanel.add(textOutput);
-	    textOutput.setReadOnly(true);
-	    
-	    
-	    // Assemble Buttons
-	    buttonPanel.add(bpmnb);
-	    buttonPanel.add(epcb);
-	    buttonPanel.add(engb);
-	    buttonPanel.add(gerb);
-	    buttonPanel.add(sendButton);
-	    
-	    // Set standard button
-	    bpmnb.setChecked(true);
-	    engb.setChecked(true);
-	    
-	    // Move cursor focus to the input box.
-	    textInput.setFocus(true);
-	    
-	    // Listen for mouse events on the Add button.
-	    sendButton.addClickHandler(new ClickHandler() {
-	      public void onClick(ClickEvent event) {
-	        sendtext();
-	      }
-	    });
-	    
-	    // Listen for keyboard events in the input box.
-	    textInput.addKeyDownHandler(new KeyDownHandler() {
-	      public void onKeyDown(KeyDownEvent event) {
-	        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-	          sendtext();
-	        }
-	      }
-	    });
-	
+		// Assemble send panel.
+		mainPanel.add(textInput);
+		textInput.setCharacterWidth(100);
+		textInput.setVisibleLines(25);
+
+		mainPanel.add(buttonPanel);
+
+		// Assemble output panel.
+		mainPanel.add(outputPanel);
+		textOutput.setCharacterWidth(100);
+		textOutput.setVisibleLines(25);
+
+		outputPanel.add(textOutput);
+
+		// set Output readonly
+		textOutput.setReadOnly(true);
+
+		// Assemble Buttons
+		buttonPanel.add(bpmnb);
+		buttonPanel.add(epcb);
+		buttonPanel.add(engb);
+		buttonPanel.add(gerb);
+		buttonPanel.add(sendButton);
+
+		// Set standard button
+		bpmnb.setChecked(true);
+		engb.setChecked(true);
+
+		// Move cursor focus to the input box.
+		textInput.setFocus(true);
+
+		// Listen for mouse events on the Add button.
+		sendButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				sendtext();
+			}
+		});
+
+		// Listen for keyboard events in the input box.
+		textInput.addKeyDownHandler(new KeyDownHandler() {
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					sendtext();
+				}
+			}
+		});
+
 	}
-	
-	/**  * Save input text as string */ 
+
+	// Save input text as string
+	@SuppressWarnings("deprecation")
 	private void sendtext() {
-	    String text = textInput.getText();
-	    textInput.setFocus(true);
-	    
-	    // Save the modeltype in t // 1 = BPMN; 0 = EPK
-	    if (bpmnb.isChecked()) {
-	    	t = 1;
-	    } else {
-	    	t = 0 ;
-	    }
-	    
-	    // Save the language in l // 1 = english; 0 = german
-	    if (engb.isChecked()) {
-	    	l = 1;
-	    } else {
-	    	l = 0 ;
-	    }
-	    
-	    String ss;
-	    String sl;
-	    
-	    if (l==1) {ss="english";} else {ss="Deutsch";}
-	    if (t==1) {sl="BPMN";} else {sl="EPK";}
-	    
-        messageService.getMessage(textInput.getValue(),new MessageCallBack());
-	    
-	 }
-	
+		textInput.setFocus(true);
+
+		// Save the modeltype in t // 1 = BPMN; 0 = EPK
+		if (bpmnb.isChecked()) {
+			t = 1;
+		} else {
+			t = 0;
+		}
+
+		// Save the language in l // 1 = english; 0 = german
+		if (engb.isChecked()) {
+			l = 1;
+		} else {
+			l = 0;
+		}
+
+		// do the call
+		messageService.getMessage(textInput.getValue(), new MessageCallBack());
+
+	}
+
 }
--- src/com/winfo/text2model/client/Text2Model.java
+++ src/com/winfo/text2model/client/Text2Model.java
@@ -1,30 +1,18 @@
 package com.winfo.text2model.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /** * Entry point classes define <code>onModuleLoad()</code>. */
 public class Text2Model implements EntryPoint {
 
-	
-	
-/**  * Entry point method.  */  
-public void onModuleLoad() {
+	/** * Entry point method. */
+	public void onModuleLoad() {
 
-	Gui gui = new Gui();
-	
-	RootPanel.get().add(gui);
+		Gui gui = new Gui();
 
-}
+		RootPanel.get().add(gui);
+
+	}
 
 }
